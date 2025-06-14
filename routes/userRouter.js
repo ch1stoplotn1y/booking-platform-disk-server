86fe080d5d531cd1b72da53bf356a95d33d2ca88
menthol_dota <sh4rk0@bk.ru>
2025-06-14 15:30:30 +0300

    wqe

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,11 +7,15 @@ const userRouter = new Router();
 //Ендпоинты для админ панели
 userRouter.get("/all", checkRoleFunction("admin"), userController.getAllUsers);
 userRouter.put("/:id", checkRoleFunction("admin"), userController.updateUser);
+userRouter.delete(
+    "/:id",
+    checkRoleFunction("admin"),
+    userController.deleteUser
+);
 
 userRouter.get("/me", authFunction, userController.getUserProfile);
 userRouter.patch("/me/avatar", authFunction, userController.updateAvatar);
 userRouter.patch("me/avatar/delete", userController.deleteAvatar);
-userRouter.delete("/me", authFunction, userController.deleteUser);
 userRouter.get(
     "/me/properties",
     authFunction,
